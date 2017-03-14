66a467765977d1f67f28c348c813945a8ba54426
HideakiTouhara <hideee6723@gmail.com>
2017-03-14 18:19:15 +0900

    Create RoomSelectCell.cs

---
+++ Assets/Script/Photon/RoomSelectCell.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomSelectCell : MonoBehaviour {
+
+	[SerializeField]
+	private Text
+		nameLabel,
+		playerCountLabel;
+
+	[SerializeField]
+	private Button
+		joinRoomButton;
+
+	private RoomInfo roomInfo;
+
+	public void SetValue(RoomInfo room)
+	{
+		roomInfo = room;
+		nameLabel.text = room.name;
+		playerCountLabel.text = room.playerCount.ToString();
+		joinRoomButton.onClick.AddListener(PushJoinRoomButton);
+	}
+
+	private void PushJoinRoomButton()
+	{
+		ConnectionManager.instance.JoinRoom(roomInfo.name);
+	}
+}
