13502a1ac37ef76f73707aea70ee687608dc68ae
HideakiTouhara <hideee6723@gmail.com>
2017-03-14 18:20:34 +0900

    Create RoomInfoPanel.cs

---
+++ Assets/Script/Photon/RoomInfoPanel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomInfoPanel : MonoBehaviour {
+
+	[SerializeField]
+	private Text infoLabel;
+
+	[SerializeField]
+	private Button startButton;
+
+	Room room;
+
+	// Use this for initialization
+	void Start () 
+	{
+		startButton.onClick.AddListener(() => {
+			GameManager.instance.LoadScene(Const.Scene.testscene);
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(PhotonNetwork.inRoom)
+		{
+			if(room == null)
+				room = PhotonNetwork.room;
+			string info =
+				"room name : " + room.name + "\n"
+				+ "player count : " + room.playerCount + "/" + room.maxPlayers + "人\n";
+			infoLabel.text = info;
+		}
+	}
+}
